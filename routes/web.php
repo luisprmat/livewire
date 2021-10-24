e4f5f088a032435f3b4a403e615f79135a9bfc6f
Luis Antonio Parrado <luisprmat@gmail.com>
2021-10-24 11:00:52 -0500

    User search without Livewire

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'users' => User::all()
+    ]);
+});
+
+Route::get('search', function (Request $request) {
+    return view('partials.users', [
+        'users' => User::where('name', 'LIKE', "%{$request->q}%")->get()
+    ]);
 });
