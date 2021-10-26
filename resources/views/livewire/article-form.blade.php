c5dfa50ff4497d86b88dcad7f8ca9184d38a4ffd
Luis Antonio Parrado <luisprmat@gmail.com>
2021-10-25 19:43:55 -0500

    Styling form with Tailwind CSS

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -1,18 +1,45 @@
 <div>
-    <h1>Crear artículo</h1>
-    <form wire:submit.prevent="save">
-        <label>
-            <input wire:model="article.title" type="text" placeholder="Título">
-            @error('article.title') <div>{{ $message }}</div> @enderror
-        </label>
-        <label>
-            <input wire:model="article.slug" type="text" placeholder="Url amigable">
-            @error('article.slug') <div>{{ $message }}</div> @enderror
-        </label>
-        <label>
-            <textarea wire:model="article.content" placeholder="Contenido"></textarea>
-            @error('article.content') <div>{{ $message }}</div> @enderror
-        </label>
-        <input type="submit" value="Guardar">
-    </form>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('New article') }}
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <x-jet-form-section submit="save">
+                <x-slot name="title">
+                    {{ __('New article') }}
+                </x-slot>
+                <x-slot name="description">
+                    {{ __('Create a new article') }}
+                </x-slot>
+
+                <x-slot name="form">
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-jet-label for="title" :value="__('Title')" />
+                        <x-jet-input wire:model="article.title" id="title" class="mt-1 w-full" type="text" />
+                        <x-jet-input-error for="article.title" class="mt-2" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-jet-label for="slug" :value="__('Slug')" />
+                        <x-jet-input wire:model="article.slug" id="slug" class="mt-1 w-full" type="text" />
+                        <x-jet-input-error for="article.slug" class="mt-2" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-jet-label for="content" :value="__('Content')" />
+                        <x-textarea wire:model="article.content" id="content" class="mt-1 w-full" />
+                        <x-jet-input-error for="article.content" class="mt-2" />
+                    </div>
+                </x-slot>
+
+                <x-slot name="actions">
+                    <x-jet-button>
+                        {{ __('Save') }}
+                    </x-jet-button>
+                </x-slot>
+
+            </x-jet-form-section>
+        </div>
+    </div>
 </div>
