6a2d924d045d3fe80267c4fd542b26ec10114a1a
Luis Antonio Parrado <luisprmat@gmail.com>
2021-11-08 09:40:37 -0500

    Styling image selector with Tailwind CSS

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -16,9 +16,19 @@
                 </x-slot>
 
                 <x-slot name="form">
-                    <div class="col-span-6 sm:col-span-4">
-                        <x-jet-label for="image" :value="__('Image')" />
-                        <x-jet-input wire:model="image" id="image" class="mt-1 w-full" type="file" />
+                    <div class="col-span-6 sm:col-span-4 relative">
+                        @if ($image)
+                            <x-jet-danger-button wire:click="$set('image')" class="absolute bottom-2 right-2">{{ __('Change Image') }}</x-jet-danger-button>
+                            <img class="border-2 rounded" src="{{ $image?->temporaryUrl() }}" alt="Image">
+                        @elseif ($article->image)
+                            <x-jet-label for="image" :value="__('Change Image')" class="absolute bottom-2 right-2 cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition" />
+                            <img class="border-2 rounded" src="{{ Storage::disk('public')->url($article->image) }}" alt="Image">
+                        @else
+                            <div class="h-32 bg-gray-50 border-2 border-dashed rounded flex items-center justify-center">
+                                <x-jet-label for="image" :value="__('Select Image')" class="cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition" />
+                            </div>
+                        @endif
+                        <x-jet-input wire:model="image" id="image" class="hidden" type="file" />
                         <x-jet-input-error for="image" class="mt-2" />
                     </div>
                     <div class="col-span-6 sm:col-span-4">
