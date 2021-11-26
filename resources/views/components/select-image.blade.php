386f8423218fa7043989a7388901e04bee47082c
Luis Antonio Parrado <luisprmat@gmail.com>
2021-11-25 21:37:03 -0500

    Improving accesibility of image selector

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -1,19 +1,34 @@
-<div class="relative">
-    @php($id = $attributes->wire('model')->value)
+@php($id = $attributes->wire('model')->value)
 
+<div x-data="{ focused: false }" class="relative">
     @if ($image instanceof Livewire\TemporaryUploadedFile)
         <x-jet-danger-button wire:click="$set('{{ $id }}')" class="absolute bottom-2 right-2">
             {{ __('Change Image') }}
         </x-jet-danger-button>
         <img class="border-2 rounded" src="{{ $image?->temporaryUrl() }}" alt="Image">
     @elseif ($existing)
-        <x-jet-label :for="$id" :value="__('Change Image')" class="absolute bottom-2 right-2 cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition" />
+        <label :for="$id"
+            class="absolute bottom-2 right-2 cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 disabled:opacity-25 transition"
+            :class="{'outline-none border-gray-900 ring ring-gray-300': focused}"
+        >{{ __('Change Image') }}</label>
         <img class="border-2 rounded" src="{{ Storage::disk('public')->url($existing) }}" alt="Image">
     @else
         <div class="h-32 bg-gray-50 border-2 border-dashed rounded flex items-center justify-center">
-            <x-jet-label :for="$id" :value="__('Select Image')" class="cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition" />
+            <label :for="$id"
+                class="cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 disabled:opacity-25 transition"
+                :class="{'outline-none border-gray-900 ring ring-gray-300': focused}"
+            >{{ __('Select Image') }}</label>
         </div>
     @endif
 
-    <x-jet-input wire:model="{{ $id }}" :id="$id" class="hidden" type="file" />
+    @unless ($image)
+        <x-jet-input
+            wire:model="{{ $id }}"
+            x-on:focus="focused = true"
+            x-on:blur="focused = false"
+            :id="$id"
+            class="sr-only"
+            type="file"
+        />
+    @endunless
 </div>
