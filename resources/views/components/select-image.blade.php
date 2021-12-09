f297c9f4f4aa2dc6bdffac94c895bd861c3ecca7
Luis Antonio Parrado <luisprmat@gmail.com>
2021-12-08 21:52:17 -0500

    Correcting errors in the form

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -7,14 +7,14 @@
         </x-jet-danger-button>
         <img class="border-2 rounded" src="{{ $image?->temporaryUrl() }}" alt="Image">
     @elseif ($existing)
-        <label :for="$id"
+        <label for="{{ $id }}"
             class="absolute bottom-2 right-2 cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 disabled:opacity-25 transition"
             :class="{'outline-none border-gray-900 ring ring-gray-300': focused}"
         >{{ __('Change Image') }}</label>
         <img class="border-2 rounded" src="{{ Storage::disk('public')->url($existing) }}" alt="Image">
     @else
         <div class="h-32 bg-gray-50 border-2 border-dashed rounded flex items-center justify-center">
-            <label :for="$id"
+            <label for="{{ $id }}"
                 class="cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 disabled:opacity-25 transition"
                 :class="{'outline-none border-gray-900 ring ring-gray-300': focused}"
             >{{ __('Select Image') }}</label>
